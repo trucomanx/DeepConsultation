e99c9ca91c0b6c1a8dad3522f885263dac9a68f6
Fernando Pujaico Rivera <fernando.pujaico.rivera@gmail.com>
2025-07-04 14:22:09 -0300

    ordenando

--- src/deep_consultation/core.py
+++ src/deep_consultation/core.py
@@ -2,14 +2,14 @@ from openai import OpenAI
 
 def consult_with_deepchat(base_url,api_key,model,user_msg,system_msg):
 
-    openai = OpenAI(
+    client = OpenAI(
         api_key=api_key,
         base_url=base_url,
     )
 
     stream = True # or False
 
-    chat_completion = openai.chat.completions.create(
+    chat_completion = client.chat.completions.create(
         model=model,
         messages=[
             {"role": "system", "content": system_msg},
@@ -18,19 +18,30 @@ def consult_with_deepchat(base_url,api_key,model,user_msg,system_msg):
         stream=stream,
     )
 
-    OUT='';
+    OUT = ""
     if stream:
         for event in chat_completion:
             if event.choices[0].finish_reason:
-                pass
-                #print(event.choices[0].finish_reason,
-                #      event.usage.prompt_tokens,
-                #      event.usage.completion_tokens)
-            else:
-                OUT=OUT+event.choices[0].delta.content
-    #else:
-    #    print(chat_completion.choices[0].message.content)
-    #    print(chat_completion.usage.prompt_tokens, chat_completion.usage.completion_tokens)
-
+                continue
+            delta = event.choices[0].delta
+            if hasattr(delta, "content") and delta.content:
+                OUT += delta.content
+    else:
+        OUT = chat_completion.choices[0].message.content
 
     return OUT
+    
+
+if __name__ == "__main__":
+    # from deep_consultation import consult_with_deepchat
+    api_key = input("Digite sua API key: ")
+    base_url = "https://api.deepinfra.com/v1/openai"  
+    model = "meta-llama/Meta-Llama-3.1-70B-Instruct" 
+
+    user_msg = "Escreve um conto muito longo, o maior que vc possa"
+    system_msg = "Você é um assistente útil que responde de forma clara e objetiva."
+
+    resposta = consult_with_deepchat(base_url, api_key, model, user_msg, system_msg)
+
+    print("\nResposta do modelo:\n")
+    print(resposta)
--- src/setup.py
+++ src/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="deep-consultation",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     author="Fernando Pujaico Rivera",
     author_email="fernando.pujaico.rivera@gmail.com",
